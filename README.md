0da1199804109f209fab4a1ec620839a04f58b98
Miguel Peixe <miguel@cardume.art.br>
2015-02-24 14:22:41 -0300

    remove lib and create server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CEP
+
+API server based on [cep-scraper](https://github.com/codigourbano/cep-scraper).
--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-var scrape = require('./scrape');
+var express = require('express'),
+	cep = require('cep-scraper');
 
-scrape('barra funda', function(data) {
-	console.log(data);
-})
+var app = express();
+
+app.all('/', function(req, res) {
+	if(req.query.search) {
+		cep.scrape(req.query.search, function(data) {
+			res.send(data);
+		});
+	} else {
+		res.send({'error': 'Missing search query.'});
+	}
+
+});
+
+var port = process.env.PORT || 8000;
+
+app.listen(port, function() {
+	console.log('Listening on port ' + port);
+});
