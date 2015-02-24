dd53bb07e8c7674fec1be736bb3546fcba74b33d
Miguel Peixe <miguel@cardume.art.br>
2015-02-24 17:12:51 -0300

    support query

--- index.js
+++ index.js
@@ -5,11 +5,27 @@ var app = express();
 
 app.all('/', function(req, res) {
 	if(req.query.search) {
-		cep.scrape(req.query.search, function(data) {
-			res.send(data);
+		cep.scrape(req.query.search, function(response) {
+			if(response.error) {
+				res.status(404);
+			}
+			res.send(response);
+		});
+	} else if(req.query.uf) {
+		cep.scrape({
+			uf: req.query.uf,
+			localidade: req.query.localidade,
+			tipo: req.query.tipo,
+			logradouro: req.query.logradouro,
+			numero: req.query.numero
+		}, function(response) {
+			if(response.error) {
+				res.status(404);
+			}
+			res.send(response);
 		});
 	} else {
-		res.send({'error': 'Missing search query.'});
+		res.status(400).send({'error': 'Missing search query.'});
 	}
 
 });
